0c21e9fd9f0fcb0212bbd8e39163a0b389efeff2
Sam Ward <sam.ward@flauntdigital.com>
2021-07-30 13:35:15 +0100

    Add products page

---
+++ src/pages/products.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+import { Helmet } from 'react-helmet'
+import styles from './blog.module.css'
+import Layout from '../components/layout'
+
+class BlogIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const posts = get(this, 'props.data.allContentfulProduct.edges')
+
+    return (
+      <Layout location={this.props.location}>
+        <div style={{ background: '#fff' }}>
+          <Helmet title={siteTitle} />
+          <div className={styles.hero}>Products</div>
+          <div className="wrapper">
+            <h2 className="section-headline">Recent articles</h2>
+            <ul className="article-list">
+              {posts.map(({ node }) => {
+                return (
+                  <li key={node.name}>
+                    <p>{node.name}</p>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query ProductIndexQuery {
+    allContentfulProduct {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`
